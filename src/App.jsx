920fe5c563700880b3c3ee0a6f026bee8ab90d4e
pedroceleste351-lab <pedroceleste351@gmail.com>
2025-08-05 19:22:13 -0300

    Simplifica App.jsx para deploy básico

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import AddAnimal from "./pages/AddAnimal";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gradient-to-br from-yellow-100 to-orange-100 text-gray-800 font-sans">
-        <Navbar />
-        <main className="p-4 max-w-6xl mx-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/add-animal" element={<AddAnimal />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Projeto ExoticKingdom</h1>
+      <p>Deploy básico funcionando</p>
+    </div>
   );
 }
 
 export default App;
+
